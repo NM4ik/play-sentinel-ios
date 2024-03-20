fa6b2fdc474f92ca6f96acffa0699bb5999e4749
Nikita Mikhailov <n.mikhailov55@gmail.com>
2024-03-20 19:33:04 +0300

    Initial Commit

---
+++ PlaySentinel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlaySentinel
+//
+//  Created by Никита Михайлов on 20.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlaySentinel/PlaySentinelApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySentinelApp.swift
+//  PlaySentinel
+//
+//  Created by Никита Михайлов on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlaySentinelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
